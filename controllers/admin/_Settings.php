cd37c5ce4cfa951dfb9d5cead483cdb423e9ede7
jacobd <jacobd@inmotionhosting.com>
2025-10-30 19:26:44 +0000

    Fixed XSS issue.

--- controllers/admin/_Settings.php
+++ controllers/admin/_Settings.php
@@ -210,33 +210,54 @@ class SC_Settings_API extends SC_Controller {
 			// Update rewrite rules when options are updated.
 			flush_rewrite_rules();
 		}
-		if ( isset( $_GET['tab'] ) && $_GET['tab'] != '' ) {
-			$tabs = apply_filters( 'si_option_tabs', self::$option_tabs );
-			if ( isset( $tabs[ $_GET['tab'] ] ) ) {
-				$tab_args = $tabs[ $_GET['tab'] ];
-				if ( isset( $tab_args['callback'] ) && is_callable( $tab_args['callback'] ) ) {
-					call_user_func_array( $tab_args['callback'], array() );
-				} else {
-					$plugin_page = $_GET['page'];
-					$title = ( isset( $tabs['title'] ) ) ? $tabs['title'] : '' ;
-					$ajax = isset( $tabs['ajax'] )?$tabs['ajax']:'';
-					$ajax_full_page = isset( $tabs['ajax_full_page'] )?$tabs['ajax_full_page']:'';
-					$reset = isset( $tabs['reset'] )?$tabs['reset']:'';
-					$section = isset( $tabs['section'] )?$tabs['section']:'';
+		$plugin_page = '';
+		if ( isset( $_GET['page'] ) ) {
+			$raw_page = \wp_unslash( $_GET['page'] );
+			if ( \is_string( $raw_page ) ) {
+				$plugin_page = \sanitize_key( $raw_page );
+			}
+		}
 
-					self::load_view( 'admin/settings', array(
-						'title' => __( $title ),
-						'page' => $plugin_page,
-						'ajax' => $ajax,
-						'ajax_full_page' => $ajax_full_page,
-						'reset' => $reset,
-						'section' => $section,
-					), false );
+		$tab_slug = '';
+		if ( isset( $_GET['tab'] ) ) {
+			$raw_tab = \wp_unslash( $_GET['tab'] );
+			if ( \is_string( $raw_tab ) ) {
+				$tab_slug = \sanitize_key( $raw_tab );
+			}
+		}
+
+		$active_tab = '';
+		if ( '' !== $tab_slug ) {
+			$tabs = \apply_filters( 'si_option_tabs', self::$option_tabs );
+			if ( isset( $tabs[ $tab_slug ] ) ) {
+				$tab_args  = $tabs[ $tab_slug ];
+				$active_tab = $tab_slug;
+				if ( isset( $tab_args['callback'] ) && \is_callable( $tab_args['callback'] ) ) {
+					\call_user_func_array( $tab_args['callback'], array() );
+				} else {
+					$title  = ( isset( $tabs['title'] ) ) ? $tabs['title'] : '';
+					$ajax   = isset( $tabs['ajax'] ) ? $tabs['ajax'] : '';
+					$ajax_full_page = isset( $tabs['ajax_full_page'] ) ? $tabs['ajax_full_page'] : '';
+					$reset  = isset( $tabs['reset'] ) ? $tabs['reset'] : '';
+					$section = isset( $tabs['section'] ) ? $tabs['section'] : '';
+
+					self::load_view(
+						'admin/settings',
+						array(
+							'title'       => __( $title ),
+							'page'        => $plugin_page,
+							'ajax'        => $ajax,
+							'ajax_full_page' => $ajax_full_page,
+							'reset'       => $reset,
+							'section'     => $section,
+							'current_tab' => $active_tab,
+						),
+						false
+					);
 				}
 				return;
 			}
 		}
-		$plugin_page = $_GET['page'];
 		$title = ( isset( self::$admin_pages[ $plugin_page ]['title'] ) ) ? self::$admin_pages[ $plugin_page ]['title'] : '' ;
 		$ajax = isset( self::$admin_pages[ $plugin_page ]['ajax'] )?self::$admin_pages[ $plugin_page ]['ajax']:'';
 		$ajax_full_page = isset( self::$admin_pages[ $plugin_page ]['ajax_full_page'] )?self::$admin_pages[ $plugin_page ]['ajax_full_page']:'';
@@ -250,6 +271,7 @@ class SC_Settings_API extends SC_Controller {
 				'ajax_full_page' => $ajax_full_page,
 				'reset' => $reset,
 				'section' => $section,
+				'current_tab' => $active_tab,
 		), false );
 		return;
 	}
@@ -259,8 +281,29 @@ class SC_Settings_API extends SC_Controller {
 	 * @return string              html
 	 */
 	public static function display_settings_tabs( $plugin_page = 0 ) {
+		$current_page = '';
+		if ( isset( $_GET['page'] ) ) {
+			$raw_page = \wp_unslash( $_GET['page'] );
+			if ( \is_string( $raw_page ) ) {
+				$current_page = \sanitize_key( $raw_page );
+			}
+		}
+
+		$current_tab = '';
+		if ( isset( $_GET['tab'] ) ) {
+			$raw_tab = \wp_unslash( $_GET['tab'] );
+			if ( \is_string( $raw_tab ) ) {
+				$current_tab = \sanitize_key( $raw_tab );
+			}
+		}
+
+		$tabs = \apply_filters( 'si_option_tabs', self::$option_tabs );
+
 		if ( ! $plugin_page ) {
-			$plugin_page = ( self::APP_DOMAIN === $_GET['page'] ) ? self::APP_DOMAIN . '/' . self::SETTINGS_PAGE : $_GET['page'] ;
+			$plugin_page = ( self::APP_DOMAIN === $current_page ) ? self::APP_DOMAIN . '/' . self::SETTINGS_PAGE : $current_page;
+		}
+		if ( '' !== $current_tab && ! isset( $tabs[ $current_tab ] ) ) {
+			$current_tab = '';
 		}
 		if ( ! isset( self::$admin_pages[ $plugin_page ]['section'] ) ) {
 			return;
@@ -268,15 +311,14 @@ class SC_Settings_API extends SC_Controller {
 		// Section based on settings slug
 		$section = self::$admin_pages[ $plugin_page ]['section'];
 		// get all tabs and sort
-		$tabs = apply_filters( 'si_option_tabs', self::$option_tabs );
-		uasort( $tabs, array( __CLASS__, 'sort_by_weight' ) );
+		\uasort( $tabs, array( __CLASS__, 'sort_by_weight' ) );
 		// loop through tabs and build markup
 		foreach ( $tabs as $key => $data ) :
 			if ( $data['section'] === $section ) {
 				$new_title = __( $data['tab_title'] );
-				$current = ( ( isset( $_GET['tab'] ) && $_GET['tab'] === $data['slug'] ) || ( ! isset( $_GET['tab'] ) && str_replace( self::APP_DOMAIN . '/', '', $plugin_page ) == $data['slug'] ) ) ? ' nav-tab-active' : '';
-				$url = ( $data['tab_only'] ) ? add_query_arg( array( 'page' => $plugin_page, 'tab' => $data['slug'] ), 'admin.php' ) : add_query_arg( array( 'page' => $plugin_page ), 'admin.php' );
-				echo '<a href="'.$url.'" class="nav-tab'.$current.'" id="si_options_tab_'.$data['slug'].'">'.$new_title.'</a>';
+				$current = ( ( '' !== $current_tab && $current_tab === $data['slug'] ) || ( '' === $current_tab && str_replace( self::APP_DOMAIN . '/', '', $plugin_page ) == $data['slug'] ) ) ? ' nav-tab-active' : '';
+				$url = ( $data['tab_only'] ) ? \add_query_arg( array( 'page' => $plugin_page, 'tab' => $data['slug'] ), 'admin.php' ) : \add_query_arg( array( 'page' => $plugin_page ), 'admin.php' );
+				echo '<a href="' . \esc_url( $url ) . '" class="nav-tab' . $current . '" id="si_options_tab_' . \esc_attr( $data['slug'] ) . '">' . $new_title . '</a>';
 			}
 		endforeach;
 		// Add the add new buttons after the tabs
--- views/admin/settings.php
+++ views/admin/settings.php
@@ -1,28 +1,43 @@
 <?php require ABSPATH . 'wp-admin/options-head.php'; // not a general options page, so it must be included here ?>
 <?php
-	$page = ( ! isset( $_GET['tab'] ) ) ? $page : self::TEXT_DOMAIN.'/'.$_GET['tab'] ; ?>
-<div id="<?php echo $page ?>" class="wrap">
+$base_page = isset( $page ) && '' !== $page ? $page : self::TEXT_DOMAIN;
+$current_tab = isset( $current_tab ) ? $current_tab : '';
+$settings_page = ( '' !== $current_tab ) ? $base_page . '/' . $current_tab : $base_page;
+$wrap_id = $settings_page;
+$form_classes = array( 'sprout_settings_form', $wrap_id );
+
+if ( ! empty( $ajax ) ) {
+	$form_classes[] = 'ajax_save';
+}
+
+if ( ! empty( $ajax_full_page ) ) {
+	$form_classes[] = 'full_page_ajax';
+}
+
+$form_class_attribute = \implode( ' ', \array_filter( $form_classes ) );
+?>
+<div id="<?php echo esc_attr( $wrap_id ); ?>" class="wrap">
 
 	<h2 class="nav-tab-wrapper">
 		<?php do_action( 'sprout_settings_tabs' ); ?>
 	</h2>
 	<div class="clearfix">
-		<?php do_action( 'sprout_settings_page_sub_heading_'.$_GET['page'] ); ?>
+		<?php do_action( 'sprout_settings_page_sub_heading_' . $base_page ); ?>
 	</div>
 
 	<span id="ajax_saving" style="display:none" data-message="<?php _e( 'Saving...', 'sprout-invoices' ) ?>"></span>
-	<form method="post" enctype="multipart/form-data" action="<?php echo admin_url( 'options.php' ); ?>" class="sprout_settings_form <?php echo $page; if ( $ajax ) { echo ' ajax_save'; } if ( $ajax_full_page ) { echo ' full_page_ajax'; }  ?>">
-		<?php settings_fields( $page ); ?>
+	<form method="post" enctype="multipart/form-data" action="<?php echo admin_url( 'options.php' ); ?>" class="<?php echo esc_attr( $form_class_attribute ); ?>">
+		<?php settings_fields( $settings_page ); ?>
 		<table class="form-table">
-			<?php do_settings_fields( $page, 'default' ); ?>
+			<?php do_settings_fields( $settings_page, 'default' ); ?>
 		</table>
-		<?php do_settings_sections( $page ); ?>
+		<?php do_settings_sections( $settings_page ); ?>
 		<?php submit_button(); ?>
 		<?php if ( $reset ) : ?>
-			<?php submit_button( sc__( 'Reset Defaults' ), 'secondary', $page.'-reset', false ); ?>
+			<?php submit_button( sc__( 'Reset Defaults' ), 'secondary', $settings_page . '-reset', false ); ?>
 		<?php endif ?>
 	</form>
 
-	<?php do_action( 'sprout_settings_page', $page ) ?>
-	<?php do_action( 'sprout_settings_page_'.$page, $page ) ?>
+	<?php do_action( 'sprout_settings_page', $settings_page ) ?>
+	<?php do_action( 'sprout_settings_page_' . $settings_page, $settings_page ) ?>
 </div>
